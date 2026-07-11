33d8cd1eceb7f99761c29d2f3f475c8425095a45
Tanish Bansiwal <bansiwaltanish@gmail.com>
2026-07-11 15:48:25 +0530

    Initialize React frontend with Vite

--- apps/api/app/core/config.py
+++ apps/api/app/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    base_url="https://api.groq.com/openai/v1"
+
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
